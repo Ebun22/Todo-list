a5263a0217f707ef252649f51aea688ce49257f6
Ebun22 <subeydavid22@gmail.com>
2023-07-12 11:03:35 +0100

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+
+const Home = async () => {
+  return (
+   <div className="h-screen">
+      <h1 className="text-3xl sm:text-4x1 font-black tracking-wide text-center pt-20 pb-10 sm:pb-10">
+        Todo List
+      </h1>
+
+      <div className="grid place-items-center">
+          <p>
+            All todo tasks
+          </p>
+      </div>
+   </div>
+  )
+}
+
+export default Home;

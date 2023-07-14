fd3ee13e5261ac22d2115d00820aa190c05565af
Ebun22 <subeydavid22@gmail.com>
2023-07-14 18:04:15 +0100

    integrated add component

--- components/AddTodo.tsx
+++ components/AddTodo.tsx
@@ -8,21 +8,40 @@ import { useState } from "react";
 import { Input } from "./ui/Input";
 import { Label } from "./ui/Label";
 import { Textarea } from "./ui/Textarea";
-import Select from "react-tailwindcss-select";
+import Select, { Option } from "./ui/Select";
+import { Dialog, DialogContent, DialogFooter } from "./ui/Dialog";
+import { Button } from "./ui/Button";
 
-const options = [
+const options: Option[] = [
+    { value: "default_option", label: "Select an option" },
     { value: "pending", label: "Pending" },
     { value: "in_progress", label: "In Progress" },
     { value: "completed", label: "Completed" }
 ];
 
 const AddTodo = observer(() => {
+    const initial = options.find((value) => value.value);
     // // const { newTodoStore } = useStore();
     // let filteredTodos: Array<Instance<typeof TodoModel>> = newTodoStore.todos;
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-    const [status, setStatus] = useState(null);
+    const [title, setTitle] = useState<string>("");
+    const [description, setDescription] = useState<string>("");
+    const [status, setStatus] = useState<Option | null>(options[0]);
+    const [errMsg, setErrMsg] = useState<string>();
 
+    const handleAddTask = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+
+        const newTodo = {
+            id: Date.now().toString(),
+            title,
+            description,
+            status: status?.value ?? " ",
+        };
+
+        newTodoStore.addTodo(newTodo)
+
+        console.log(newTodoStore)
+    }    
 
     const handleTitle = (e: any) => {
         setTitle(e.target.value)
@@ -32,11 +51,13 @@ const AddTodo = observer(() => {
         setDescription(e.target.value)
         console.log(e.target.value);
     }
-    const handleStatus = (value: any) => {
-        setStatus(value)
-        console.log(value);
+    const handleStatus = (e: any) => {
+        setStatus(e)
+        console.log(e);
     }
     return (
+        //  <DialogContent>
+        //  <Dialog>
         <form>
             <div className="grid gap-4 py-4">
                 <div className="grid grid-cols-4 items-center gap-2">
@@ -75,13 +96,17 @@ const AddTodo = observer(() => {
                         Status
                     </Label>
                     <Select
-                        value={status}
-                        onChange={handleStatus}
-                        options={options} 
-                        primaryColor={"sky"}                    />
+                        selectedOption={status}
+                        handelChange={(e: any) => handleStatus(e)}
+                        options={options}
+                    />
                 </div>
+                <DialogFooter>
+                    <Button type="submit" onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => handleAddTask(e)}>Save Task</Button>
+                </DialogFooter>
             </div>
         </form>
+        
     )
 })
 
--- components/ui/Select.tsx
+++ components/ui/Select.tsx
@@ -1,119 +1,144 @@
-// "use client"
+// // External Dependencies
+import React, { FC } from "react";
+import { Listbox, Transition } from "@headlessui/react";
 
-// import * as React from "react"
-// import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons"
-// import {SelectPrimitive} from "@radix-ui/react-select"
-// import { cn } from "@/lib/utils"
+// // style
+// import "./select.css";
 
-// const Select = SelectPrimitive.Root
+export interface Option {
+    value: string;
+    label: string;
+  }
 
-// const SelectGroup = Select.Group
+  interface SelectProps {
+    options: Option[];
+    selectedOption: Option | null;
+    handelChange: (option: Option | null) => void;
+  }
+  
+const Selector: FC = () => (
+  <svg
+    className="h-5 w-5 text-gray-400"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+    aria-hidden="true"
+  >
+    <path
+      fillRule="evenodd"
+      d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
 
-// const SelectValue = SelectPrimitive.Value
+const Selected: FC = () => (
+  <svg
+    className="h-5 w-5"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+    />
+  </svg>
+);
 
-// const SelectTrigger = React.forwardRef<
-//   React.ElementRef<typeof SelectPrimitive.Trigger>,
-//   React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
-// >(({ className, children, ...props }, ref) => (
-//   <SelectPrimitive.Trigger
-//     ref={ref}
-//     className={cn(
-//       "flex h-9 w-full items-center justify-between rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-//       className
-//     )}
-//     {...props}
-//   >
-//     {children}
-//     <SelectPrimitive.Icon asChild>
-//       <CaretSortIcon className="h-4 w-4 opacity-50" />
-//     </SelectPrimitive.Icon>
-//   </SelectPrimitive.Trigger>
-// ))
-// SelectTrigger.displayName = SelectPrimitive.Trigger.displayName
+const Select: FC<SelectProps> = ({
+  /* label, */
+  options,
+  selectedOption,
+  handelChange
+}) => {
+  return (
+    <Listbox
+      as="div"
+      //className={className}
+      value={selectedOption}
+      onChange={(event) => {
+        console.log("enfant", event);
+        handelChange(event);
+      }}
+    >
+      {({ open }) => (
+        <>
+          {/*label && (
+            <Listbox.Label className="mb-1 text-sm font-medium text-blue-gray-500">
+              {label}
+            </Listbox.Label>
+          )*/}
+          <div className="relative mt-1">
+            <span className="inline-block w-full rounded-md shadow-sm">
+              <Listbox.Button className="cursor-default relative w-full rounded-md border border-gray-300 bg-white pl-3 pr-10 py-2 text-left  transition ease-in-out duration-150">
+                <span className="block truncate">{selectedOption?.label}</span>
+                <span className="absolute inset-y-0 right-0 flex items-center pr-2 ml-3 pointer-events-none">
+                  <Selector />
+                </span>
+              </Listbox.Button>
+            </span>
+            <div className="absolute z-10 w-full mt-1 bg-white rounded-md shadow-lg mb-11">
+              {/* bottom-0 will open the select menu up & mb-11 will put the dropup above the select option */}
+              <Transition
+                show={open}
+                leave="transition duration-100 ease-in"
+                leaveFrom="transform opacity-100"
+                leaveTo="transform opacity-0"
+              >
+                <Listbox.Options
+                  static
+                  className="py-1 overflow-auto text-base rounded-md max-h-56 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+                >
+                  {options.map((option, index) => {
+                    return (
+                      <Listbox.Option
+                        as={React.Fragment}
+                        key={index}
+                        value={option}
+                      >
+                        {({ active, selected }) => {
+                          return (
+                            <li
+                              className={`${
+                                active
+                                  ? "text-white bg-indigo-600"
+                                  : "text-gray-900"
+                              } cursor-default select-none relative py-2 pl-3 pr-9`}
+                            >
+                              <div className="flex items-center">
+                                <span
+                                  className={`${
+                                    selected ? "font-semibold" : "font-normal"
+                                  } flex items-center block truncate`}
+                                >
+                                  {option.label}
+                                </span>
+                                {selected && (
+                                  <span
+                                    className={`${
+                                      active ? "text-white" : "text-indigo-600"
+                                    } absolute inset-y-0 right-0 flex items-center mr-3 pl-1.5`}
+                                  >
+                                    <Selected />
+                                  </span>
+                                )}
+                              </div>
+                            </li>
+                          );
+                        }}
+                      </Listbox.Option>
+                    );
+                  })}
+                </Listbox.Options>
+              </Transition>
+            </div>
+          </div>
+        </>
+      )}
+    </Listbox>
+  );
+};
 
-// const SelectContent = React.forwardRef<
-//   React.ElementRef<typeof SelectPrimitive.Content>,
-//   React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
-// >(({ className, children, position = "popper", ...props }, ref) => (
-//   <SelectPrimitive.Portal>
-//     <SelectPrimitive.Content
-//       ref={ref}
-//       className={cn(
-//         "relative z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
-//         position === "popper" &&
-//           "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
-//         className
-//       )}
-//       position={position}
-//       {...props}
-//     >
-//       <SelectPrimitive.Viewport
-//         className={cn(
-//           "p-1",
-//           position === "popper" &&
-//             "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
-//         )}
-//       >
-//         {children}
-//       </SelectPrimitive.Viewport>
-//     </SelectPrimitive.Content>
-//   </SelectPrimitive.Portal>
-// ))
-// SelectContent.displayName = SelectPrimitive.Content.displayName
-
-// const SelectLabel = React.forwardRef<
-//   React.ElementRef<typeof SelectPrimitive.Label>,
-//   React.ComponentPropsWithoutRef<typeof SelectPrimitive.Label>
-// >(({ className, ...props }, ref) => (
-//   <SelectPrimitive.Label
-//     ref={ref}
-//     className={cn("px-2 py-1.5 text-sm font-semibold", className)}
-//     {...props}
-//   />
-// ))
-// SelectLabel.displayName = SelectPrimitive.Label.displayName
-
-// const SelectItem = React.forwardRef<
-//   React.ElementRef<typeof SelectPrimitive.Item>,
-//   React.ComponentPropsWithoutRef<typeof SelectPrimitive.Item>
-// >(({ className, children, ...props }, ref) => (
-//   <SelectPrimitive.Item
-//     ref={ref}
-//     className={cn(
-//       "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-8 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
-//       className
-//     )}
-//     {...props}
-//   >
-//     <span className="absolute right-2 flex h-3.5 w-3.5 items-center justify-center">
-//       <SelectPrimitive.ItemIndicator>
-//         <CheckIcon className="h-4 w-4" />
-//       </SelectPrimitive.ItemIndicator>
-//     </span>
-//     <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
-//   </SelectPrimitive.Item>
-// ))
-// SelectItem.displayName = SelectPrimitive.Item.displayName
-
-// const SelectSeparator = React.forwardRef<
-//   React.ElementRef<typeof SelectPrimitive.Separator>,
-//   React.ComponentPropsWithoutRef<typeof SelectPrimitive.Separator>
-// >(({ className, ...props }, ref) => (
-//   <SelectPrimitive.Separator
-//     ref={ref}
-//     className={cn("-mx-1 my-1 h-px bg-muted", className)}
-//     {...props}
-//   />
-// ))
-// SelectSeparator.displayName = SelectPrimitive.Separator.displayName
-
-// export {
-//   Select,
-//   SelectGroup,
-//   SelectValue,
-//   SelectTrigger,
-//   SelectContent,
-//   SelectLabel,
-//   SelectItem,
-//   SelectSeparator,
-// }
+export default Select;
